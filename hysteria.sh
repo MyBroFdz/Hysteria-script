984a63f1fc0316a5957611637f865c5d4f790720
MyBroFdz <112946532+MyBroFdz@users.noreply.github.com>
2023-09-16 13:47:43 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -105,7 +105,7 @@ downloadHysteria() {
     rm -rf /root/Hysteria
     mkdir /root/Hysteria
     ## last_version=$(curl -Ls "https://api.github.com/repos/HyNetwork/Hysteria/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    last_version=$(curl -Ls "https://data.jsdelivr.com/v1/package/resolve/gh/HyNetwork/Hysteria" | grep '"version":' | sed -E 's/.*"([^"]+)".*/\1/')
+    last_version=1.2.1
     if [[ ! -n "$last_version" ]]; then
         red "检测 Hysteria 版本失败，可能是网络错误，请稍后再试"
         exit 1
